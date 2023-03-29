d8b6b03b6a7423133ec38962c46978c77b421749
unknown <3shk1kudale@gmail.com>
2023-03-29 17:24:22 +0530

    Front end added

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express=require("express")
 const PORT=process.env.PORT||3000;
 
+
 const app =express()
+app.set('view engine', 'ejs');
 
 app.get("/",(req,res)=>{
     res.send("testing")
+})
+
+app.get("/index",(req,res)=>{
+    res.render("pages/index");
 })
 
 app.listen(PORT,()=>{
